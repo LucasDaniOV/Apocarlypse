23baf15ff17a0b1a13f005092067c849c4a3c114
TimV1503 <tim.vereecke@student.kuleuven.be>
2022-11-28 13:43:55 +0100

    cleaning up

--- classes.py
+++ classes.py
@@ -10,14 +10,7 @@ class player:
 
     def draw(self, screen):
         pygame.draw.circle(screen, (255, 0, 0), (self.x, self.y), 50)
-    
-    def ChangeX(self, x):
-        self.x += x
-    
-    def ChangeY(self, y):
-        self.y += y
-
-
+        
 class keyboard:
     def __init__(self):
         self.keys = pygame.key.get_pressed()
