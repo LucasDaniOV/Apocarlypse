b341c1de11c23aea5611b74fb1b893993a93a88f
TimV1503 <tim.vereecke@student.kuleuven.be>
2022-11-29 13:35:45 +0100

    comments

--- classes.py
+++ classes.py
@@ -56,7 +56,7 @@ class player:
         self.__x += x
         self.__y += y
         self.__rect = self.__image.get_rect(topleft=(self.__x, self.__y))
-        
+       
     
     def get_x(self):
         return self.__x
--- functions.py
+++ functions.py
@@ -5,5 +5,7 @@ from pygame.locals import *
 from pygame.display import flip
 from classes import *
 
+# Contains helper functions
+
 def is_inside(rect1, rect2):
     return rect1.contains(rect2)
