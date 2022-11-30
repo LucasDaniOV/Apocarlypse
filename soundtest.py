304422c9d015bcf339915c63be5516612886538b
MengMassart_UCLL <r0891147@ucll.be>
2022-11-30 11:43:09 +0100

    Working on random explosions

---
+++ soundtest.py
@@ -0,0 +1,97 @@
+import pygame
+import time
+import os
+import glob
+import string
+import re
+import random
+
+pygame.mixer.init()
+
+# pygame.mixer.music.load('sounds/ost/gasgasgas.ogg')
+# pygame.mixer.music.play()
+
+class sound_library:
+    def __init__(self, path):
+        self.sounds=[]
+        self.table=self.createTable(path)
+        # print(self.table)
+    
+    def playsound(self, ID):
+        sfx = pygame.mixer.Sound(self.table[ID])
+        pygame.mixer.Sound.play(sfx)
+
+    def addsound(self, name, location):
+        self.sounds[name]=location
+
+    def find_audio_files(self):
+        res = []
+
+        for root, dirs, files in os.walk(r"./sounds"):
+            for file in files:
+                if file.endswith(".wav"):
+                    res.append(os.path.join(root, file))
+                elif file.endswith(".ogg"):
+                    res.append(os.path.join(root, file))
+                elif file.endswith(".mp3"):
+                    res.append(os.path.join(root, file))
+
+        res = [e.replace("\\", "/") for e in res]
+
+        # print(res)
+        return res    
+
+    def derive_id(self):
+        res = []
+
+        for root, dirs, files in os.walk(r"./sounds"):
+            for file in files:
+                if file.endswith(".wav"):
+                    res.append(os.path.join(root, file))
+                elif file.endswith(".ogg"):
+                    res.append(os.path.join(root, file))
+                elif file.endswith(".mp3"):
+                    res.append(os.path.join(root, file))
+
+        res = [e.replace("\\", "/") for e in res]
+        res = [e.replace("/sounds", "") for e in res]
+        res = [e.replace("./", "") for e in res]
+        res = [e.replace(".wav", "") for e in res]
+        res = [e.replace(".ogg", "") for e in res]
+        res = [e.replace(".mp3", "") for e in res]
+
+        # print(res)
+        return res
+
+    def createTable(self, path):
+        table={}
+
+        for (path, ID) in zip(self.find_audio_files(),self.derive_id()):
+            table[ID]=path
+        return table
+    
+    def play_random_explosion(self):
+        res = []
+
+        for root, dirs, files in os.walk(r"./sounds"):
+            for file in files:
+                if file.endswith(".wav"):
+                    res.append(os.path.join(root, file))
+                elif file.endswith(".ogg"):
+                    res.append(os.path.join(root, file))
+                elif file.endswith(".mp3"):
+                    res.append(os.path.join(root, file))
+
+        res = [e.replace("\\", "/") for e in res]
+        index = string.find('./sounds/sfx/explosion1.wav')
+        res[index:]
+        print(res)
+
+
+
+# sfx1 = sound_library(r"./sounds")
+# sfx1.playsound("sfx/bullet_sfx")
+expl2 = sound_library(r"./sounds")
+expl2.playsound("sfx/explosion2")
+
+time.sleep(10)
